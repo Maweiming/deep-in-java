adbbb2398fcb543486ed084419765669e769adef
coderma <mawm@fshows.com>
2021-01-05 15:30:58 +0800

    修复 无法在终端进行文件上传/下载的bug

--- final-exam/src/main/java/com/maweiming/ftp/core/command/RetrCommand.java
+++ final-exam/src/main/java/com/maweiming/ftp/core/command/RetrCommand.java
@@ -7,9 +7,11 @@ package com.maweiming.ftp.core.command;
 import com.maweiming.ftp.core.NettpAttrKeys;
 import com.maweiming.ftp.core.util.CommandUtils;
 import io.netty.channel.ChannelHandlerContext;
-import sun.net.ftp.FtpClient;
+import org.apache.commons.io.IOUtils;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 /**
@@ -26,22 +28,16 @@ public class RetrCommand extends AbstractFtpCommand {
     @Override
     public void execute(ChannelHandlerContext ctx, String param) {
         Socket socket = ctx.attr(NettpAttrKeys.SOCKET).get();
-        PrintWriter printWriter = ctx.attr(NettpAttrKeys.PRINT_WRITER).get();
         File file = new File(CommandUtils.currDirectory + "/" + param);
         if (!file.exists()) {
             send("550 File does not exist", ctx);
         } else {
-            FtpClient.TransferType transferType = ctx.attr(NettpAttrKeys.TRANSFER_MODE).get();
-            //判断传输类型
-            if (transferType.equals(FtpClient.TransferType.BINARY)) {
-                //二进制
-                binary(ctx, socket, file);
-            } else {
-                //ASCII
-                ascii(ctx, socket, file);
-            }
+            //ctx.attr(NettpAttrKeys.TRANSFER_MODE).get();
+            //这里可以获取传输类型,暂时只使用二进制模式
+            //二进制
+            binary(ctx, socket, file);
         }
-        close(socket, printWriter);
+        close(socket, null);
     }
 
     /**
@@ -49,62 +45,18 @@ public class RetrCommand extends AbstractFtpCommand {
      */
     private void binary(ChannelHandlerContext ctx, Socket socket, File file) {
         send("150 Opening binary mode " + file.getName(), ctx);
-        BufferedOutputStream fout = null;
-        BufferedInputStream fin = null;
         try {
-            fout = new BufferedOutputStream(socket.getOutputStream());
-            fin = new BufferedInputStream(new FileInputStream(file));
-            byte[] buf = new byte[1024];
-            int i = 0;
-            while ((i = fin.read(buf, 0, 1024)) != -1) {
-                fout.write(buf, 0, i);
-            }
+            IOUtils.copy(new FileInputStream(file), socket.getOutputStream());
         } catch (Exception e) {
-            System.err.println("RetrCommand>binary,file streams operating error");
+            System.err.println("RetrCommand > IOUtils copy error");
         } finally {
             try {
-                if (null != fin) {
-                    fin.close();
-                }
-                if (null != fout) {
-                    fout.close();
-                }
+                socket.getOutputStream().close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         System.out.println("文件下载成功:" + file.getName());
-        send("226 File transfer successful.", ctx);
-    }
-
-    /**
-     * ASCII传输
-     */
-    private void ascii(ChannelHandlerContext ctx, Socket socket, File file) {
-        send("150 Opening ASCII mode " + file.getName(), ctx);
-        BufferedReader br = null;
-        PrintWriter pw = null;
-        try {
-            br = new BufferedReader(new FileReader(file));
-            pw = new PrintWriter(socket.getOutputStream(), true);
-            String s;
-            while ((s = br.readLine()) != null) {
-                pw.println(s);
-            }
-        } catch (IOException e) {
-            System.err.println("RetrCommand>ascii,file streams operating error");
-        } finally {
-            try {
-                if (null != pw) {
-                    pw.close();
-                }
-                if (null != br) {
-                    br.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
         send("226 File transfer successful.", ctx);
     }
 
--- final-exam/src/main/java/com/maweiming/ftp/core/command/StorCommand.java
+++ final-exam/src/main/java/com/maweiming/ftp/core/command/StorCommand.java
@@ -7,10 +7,13 @@ package com.maweiming.ftp.core.command;
 import com.maweiming.ftp.core.NettpAttrKeys;
 import com.maweiming.ftp.core.util.CommandUtils;
 import io.netty.channel.ChannelHandlerContext;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
-import sun.net.ftp.FtpClient;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
@@ -39,14 +42,10 @@ public class StorCommand extends AbstractFtpCommand {
             if (file.exists()) {
                 CommandUtils.send("550 File already exists", ctx);
             } else {
-                if (ctx.attr(NettpAttrKeys.TRANSFER_MODE).get().equals(FtpClient.TransferType.BINARY)) {
-
-                    binary(ctx, socket, file);
-                } else {
-                    // ASCII mode
-                    ascii(ctx, socket, file);
-                }
-
+                //ctx.attr(NettpAttrKeys.TRANSFER_MODE).get();
+                //这里可以获取传输类型,暂时只使用二进制模式
+                //二进制
+                binary(ctx, socket, file);
             }
             close(socket, printWriter);
         }
@@ -56,63 +55,19 @@ public class StorCommand extends AbstractFtpCommand {
      * 二进制传输
      */
     private void binary(ChannelHandlerContext ctx, Socket socket, File file) {
-        BufferedOutputStream bos = null;
-        BufferedInputStream bis = null;
         CommandUtils.send("150 Opening binary mode " + file.getName(), ctx);
         try {
-            bos = new BufferedOutputStream(new FileOutputStream(file));
-            bis = new BufferedInputStream(socket.getInputStream());
-            byte[] buf = new byte[1024];
-            int l = 0;
-            while ((l = bis.read(buf, 0, 1024)) != -1) {
-                bos.write(buf, 0, l);
-            }
+            IOUtils.copy(socket.getInputStream(), new FileOutputStream(file));
         } catch (Exception e) {
-            System.err.println("StorCommand>binary,file streams operating error");
+            System.err.println("StorCommand > IOUtils copy error");
         } finally {
             try {
-                if (null != bis) {
-                    bis.close();
-                }
-                if (null != bos) {
-                    bos.close();
-                }
+                socket.getInputStream().close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         System.out.println("文件上传成功 > " + file.getName());
-        send("226 File transfer successful.", ctx);
-    }
-
-    /**
-     * ASCII传输
-     */
-    private void ascii(ChannelHandlerContext ctx, Socket socket, File file) {
-        send("150 Opening ASCII mode " + file.getName(), ctx);
-        BufferedReader br = null;
-        PrintWriter pw = null;
-        try {
-            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            pw = new PrintWriter(new FileOutputStream(file), true);
-            String s;
-            while ((s = br.readLine()) != null) {
-                pw.println(s);
-            }
-        } catch (IOException e) {
-            System.err.println("StorCommand>ascii,file streams operating error");
-        } finally {
-            try {
-                if (null != pw) {
-                    pw.close();
-                }
-                if (null != br) {
-                    br.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
         send("226 File transfer successful.", ctx);
     }
 
